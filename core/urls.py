d3834aa91de25903ae721ec0c3f65ae97799a3fa
sami <admin@seattledevicerepair.com>
2023-07-19 12:21:20 -0700

    changed the media root

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('Users.urls')),
     path('api/posts/', include('Posts.urls')),
-]
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
