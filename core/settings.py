90719ce3a2c415a3e9664783bd7358216946ed9e
sami <admin@seattledevicerepair.com>
2023-07-19 00:11:47 -0700

    collect static

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,4 @@
-#Name: validate-database
+#Name: validate-matrix
 
 from pathlib import Path
 import os
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_grip.GripMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'core.urls'
 
